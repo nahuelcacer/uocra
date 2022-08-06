9bda966ed85a359a965dcbd30c7e68c9fa6b64cc
micaela <micaelacalvente@gmail.com>
2022-08-06 03:01:10 -0300

    Revert "m2"
    This reverts commit f68f6f7ba25438ab2766b034b38565b9ef2c3930.

--- uocra/settings.py
+++ uocra/settings.py
@@ -106,18 +106,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
-MEDIA_URL = "/media/"
-
-MEDIA_ROOT = BASE_DIR / "media"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
