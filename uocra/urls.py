54dda309d90d017bcf91f8a9985cb2712b484ed3
micaela <micaelacalvente@gmail.com>
2022-08-08 07:24:25 -0300

    :-)

--- uocra/urls.py
+++ uocra/urls.py
@@ -1,4 +1,4 @@
-"""ong URL Configuration
+"""uocra URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
