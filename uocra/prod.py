698d8cf7d3c2030a2f3130b5153c250f99161300
zalazarleandrodavid <zalazarleandrodavid05@gmail.com>
2022-08-12 18:53:49 -0300

    Cambio server local

--- uocra/prod.py
+++ uocra/prod.py
@@ -1,7 +1,7 @@
 from .settings import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -15,16 +15,16 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd3s3cjbn8fg5nm',
-        'USER': 'cqbkgalxxbswhv',
-        'PASSWORD': 'ff05807bbf1a51bdb07259a7223027262abacf7f52b7491f538e9be924cc89b4',
-        'HOST': 'ec2-3-213-228-206.compute-1.amazonaws.com',
-        'PORT': '5432',
-    }
-}
+# ##DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'd3s3cjbn8fg5nm',
+#         'USER': 'cqbkgalxxbswhv',
+#         'PASSWORD': 'ff05807bbf1a51bdb07259a7223027262abacf7f52b7491f538e9be924cc89b4',
+#         'HOST': 'ec2-3-213-228-206.compute-1.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
 
 import django_heroku
 django_heroku.settings(locals())
