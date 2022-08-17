6ddcdc0136bde7f19e56fa48d77c0bd33ec64404
zalazarleandrodavid <zalazarleandrodavid05@gmail.com>
2022-08-17 16:07:28 -0300

    contacto

--- uocra/views.py
+++ uocra/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 
 def Index(request):
     return render(request, 'index.html')
+
+def Index2(request):
+    return render(request, 'contacto/contac.html')
     
 
