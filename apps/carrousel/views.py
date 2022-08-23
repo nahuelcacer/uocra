7d1d8a48990919a39d7b5e2345d81be081c06984
nahuelcace <nahue_cge@hotmail.com>
2022-08-23 19:40:26 -0300

    panel de usuario

--- apps/carrousel/views.py
+++ apps/carrousel/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from apps.carrousel.models import Carrousel
 
 # Create your views here.
+
+def AgregarImg():
+
+    return render()
--- apps/usuario/models.py
+++ apps/usuario/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class Usuario(AbstractUser):
-    imagen = models.ImageField(upload_to='usuario', default='usuario/user-default.png')
+    imagen = models.ImageField(upload_to='usuario', default='usuario\default-user.png')
 
     def get_absolute_url(self):
         return reverse('index')
