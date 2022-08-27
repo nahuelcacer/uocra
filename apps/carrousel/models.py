4db5b921c31611f47941d6b94430e83d82be160b
nahuelcace <nahue_cge@hotmail.com>
2022-08-27 02:21:19 -0300

    heroku

--- apps/carrousel/models.py
+++ apps/carrousel/models.py
@@ -1,8 +1,9 @@
+from cloudinary.models import CloudinaryField
 from django.db import models
 
 # Create your models here.
 class Carrousel(models.Model):
-    imagen = models.ImageField(null=True, blank=True, upload_to='carrousel')
+    imagen = CloudinaryField(null=True, blank=True , verbose_name='Imagen')
     title = models.CharField(max_length=150, default="titulo")
 
     def __str__(self):
--- apps/noticia/models.py
+++ apps/noticia/models.py
@@ -15,7 +15,7 @@ class Categoria(models.Model):
 class Noticia(models.Model):
     titulo = models.CharField(max_length=250, null=False)
     fecha = models.DateTimeField(auto_now_add=True)
-    texto = models.TextField(null=True)    
+    texto = models.TextField(null=True)   
     resumen = models.CharField(max_length=250, null=False, default=" ")
     activo = models.BooleanField(default=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True)
