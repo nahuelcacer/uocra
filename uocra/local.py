129e1a1c7fb0fc13aecd10511eb710f593b3d7db
nahuelcace <nahue_cge@hotmail.com>
2022-08-27 02:34:58 -0300

    heroku

--- uocra/local.py
+++ uocra/local.py
@@ -11,7 +11,7 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'proyecto',
+        'NAME': 'uocra',
         'USER': 'root',
         'PASSWORD': 'root',
         'HOST':'localhost',
