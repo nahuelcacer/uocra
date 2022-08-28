8026d1d027b1eefba0ce1edac7167338ce90a6c0
zalazarleandrodavid <109039100+zalazarleandrodavid@users.noreply.github.com>
2022-08-28 19:36:18 -0300

    Reseteo de contraseña

--- urls.py
+++ urls.py
@@ -1,19 +1,44 @@
-from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
-from .views import RegistrarUsuario, ModificarUsuario, DeleteUsuario
+"""uocra URL Configuration
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path, include
+from .views import Index, Contacto
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib.auth import views as auth_views
 
-#from .views import
-app_name = 'apps.usuario'
-
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name="usuario/login.html"), name="login"),
-    path('logout/', LogoutView.as_view(),name="logout"),
-    path('addUsuario/', RegistrarUsuario.as_view() ,name="addUsuario"),
-    path('modificarUsuario/<str:pk>', ModificarUsuario.as_view() ,name="modificarUsuario"),
-    path('eliminarUsuario/<int:pk>', DeleteUsuario.as_view() ,name="eliminarUsuario"),
+    path('admin/', admin.site.urls),
+    path('', Index, name='index'),
+    path('contacto/', Contacto, name='contacto'),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('noticia/', include('apps.noticia.urls')),
+    path('usuario/', include('apps.usuario.urls')),
+    path('comentario/', include('apps.comentario.urls')),
+    path('institucional/', include('apps.institucional.urls')),
+    path('carrousel/', include('apps.carrousel.urls')),
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name ='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name ='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name ='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name ='password_reset_complete'),
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name = "reset_password.html"), name ='reset_password'),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name = "password_reset_sent.html"), name ='password_reset_done'),
     path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name = "password_reset_form.html"), name ='password_reset_confirm'),
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name = "password_reset_done.html"), name ='password_reset_complete')
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
